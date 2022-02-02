ee0b20285fcf797682ed2d73c897be1ef1c69e42
bitnik93 <Nick93sysoev@yandex.ru>
2022-02-02 18:08:47 +0300

    шлифовка критериев

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ export const html = () => {
 // Images
 export const images = () => {
   return gulp.src(['source/img/**/*.{jpg,png}', '!source/img/favicon.svg'])
-  .pipe(squoosh())
+  // .pipe(squoosh())
   .pipe(gulp.dest('build/img'))
 }
 
